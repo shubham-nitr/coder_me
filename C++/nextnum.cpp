121ff612b40bba47c629044842f7e27adc0bdb52
Shubham Kumar <121me0470@nitrkl.ac.in>
2022-12-13 00:01:36 +0000

    next num game

---
+++ C++/nextnum.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<< "Let's play a game.\nYou will tell me a number and I will tell you the next higher number. Ok?\nEnter a number: ";
+    cin >> n;
+    cout << n+1 << endl;
+    return 0;
+}
